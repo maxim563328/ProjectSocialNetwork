d9baf57e7b3cc48f16413cafc67a2c270d61df69
Maxim Filonenko <56736526+maxim563328@users.noreply.github.com>
2020-07-22 22:22:29 +0700

    Add files via upload

---
+++ TeamGames/server/static/css/bgScripts/app.js
@@ -0,0 +1,14 @@
+VANTA.WAVES({
+    el: "#bgAnimated",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xe1011,
+    shininess: 21.00,
+    waveHeight: 8.00,
+    waveSpeed: 0.80,
+    zoom: 0.96
+})
