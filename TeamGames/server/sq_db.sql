a4ab9b2fabd0e66c51fbe28ed9f3efde5515e9ed
Maxim Filonenko <56736526+maxim563328@users.noreply.github.com>
2020-09-11 10:15:15 +0700

    Добавление таблиц в БД

--- TeamGames/server/sq_db.sql
+++ TeamGames/server/sq_db.sql
@@ -15,3 +15,23 @@ title text NOT NULL,
 text text NOT NULL,
 time integer NOT NULL
 );
+
+CREATE TABLE
+IF NOT EXISTS dialog
+(
+sender_id integer NOT NULL,
+recip_id integer NOT NULL,
+chat_id integer NOT NULL,
+);
+
+CREATE TABLE
+IF NOT EXISTS message
+(
+chat_messages_id integer NOT NULL,
+chat_messages_text text NOT NULL,
+chat_messages_fk_dialog_id integer NOT NULL,
+chat_messages_fk_user_id integer NOT NULL,
+chat_messages_fk_to_user_id integer NOT NULL,
+chat_messages_isRead integer NOT NULL,
+chat_messages_time integer NOT NULL DEFAULT 0
+);
